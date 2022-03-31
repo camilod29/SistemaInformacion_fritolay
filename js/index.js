bbc1dfedeca95535eb77ba850bff9382ad22d5da
camilod29 <cadelgado@uniboyaca.edu.co>
2022-03-30 22:47:20 -0500

    pinga

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-const open_carrito= document.getElementById('open_carrito');
-const carrito= document.getElementById('carrito');
+const open= document.getElementById('open');
+const carrito_container= document.getElementById('carrito_container');
 const close_carrito= document.getElementById('close_carrito');
+
+open.addEventListener('click', () => {
+    carrito_container.classList.add('show');
+    alert('pene');
+});
+
+close.addEventListener('click', () => {
+    carrito_container.classList.remove('show');
+});
