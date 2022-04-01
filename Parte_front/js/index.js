4db1f55b9ca07cc261388ca725706803f55cc662
RichiDuque <raduque@uniboyaca.edu.co>
2022-03-31 20:16:03 -0500

    agregar producto

--- Parte_front/js/index.js
+++ Parte_front/js/index.js
@@ -1,6 +1,8 @@
 const urlProducto = "http://localhost/clase/SistemaInformacion_fritolay/parte_php/productos";
 
 let listaProductos = [];
+let idProductos = 0;
+let Producto = null;
 
 function indexProducto() {
     let response = null;
@@ -36,6 +38,45 @@ function asignarDatosTablaHtml() {
     }
     const element = document.getElementById('catalogoProducto').getElementsByTagName('div')[0];
     element.innerHTML = html;
+}
+
+function crear(){
+    let idProductos = 0;
+    let Producto = null;
+    const elementTitulo = document.getElementById('controlForm').getElementsByTagName('h2')[0];
+    elementTitulo.innerText = 'Registrar Producto';
+    document.getElementById('nombre').value= '';
+    document.getElementById('Imagen').value= '';
+    document.getElementsByClassName('popupControll')[0].classList.remove('popupControll-cerrar');
+}
+
+
+function saveDataForm(event){
+    event.preventDefault();
+    let data = '';
+    data += 'nombre=' + document.getElementById('nombre').value;
+    data += '&cantidad=' + document.getElementById('cantidad').value;
+    data += '&categoria=' + document.getElementById('categoria').value;
+    data += '&descripcion=' + document.getElementById('descripcion').value;
+    data += '&imagen=' + document.getElementById('imagen').value;
+    data += '&precio=' + document.getElementById('precio').value;
+    save(data);
+}
+
+function save(data) {
+    let reponse = null;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            reponse = JSON.parse(this.response);
+            indexProducto();
+        }
+    };
+    let param = idProductos > 0 ? '/' + idProductos : '';
+    let metodo = idProductos > 0 ? 'PUT' : 'POST';
+    xhttp.open(metodo, urlProducto + param, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(data);
 }
 
 const open= document.getElementById('open_carrito');
